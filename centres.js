c3fa96babda4b4c5e046646985ec71ed704c18b0
varenio <info.bdec@gmail.com>
2021-01-19 11:45:17 +0100

    Add files via upload

---
+++ centres.js
@@ -0,0 +1,24 @@
+var centres = {
+"type": "FeatureCollection",
+"name": "centres",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "CENTRE": "Escola Collaso i Gil", "TITULAR": "Pública", "LAT": 41.376217, "LON": 2.168985 }, "geometry": { "type": "Point", "coordinates": [ 2.168985, 41.376217 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Escola de Bosc de Montjuïc", "TITULAR": "Pública", "LAT": 41.369209, "LON": 2.161062 }, "geometry": { "type": "Point", "coordinates": [ 2.161062, 41.369209 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Escola Ferran Sunyer", "TITULAR": "Pública", "LAT": 41.375936, "LON": 2.163198 }, "geometry": { "type": "Point", "coordinates": [ 2.163198, 41.375936 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Escola La Muntanyeta", "TITULAR": "Pública", "LAT": 41.37051, "LON": 2.148214 }, "geometry": { "type": "Point", "coordinates": [ 2.148214, 41.37051 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Escola Mossèn Jacint Verdaguer", "TITULAR": "Pública", "LAT": 41.37332, "LON": 2.154832 }, "geometry": { "type": "Point", "coordinates": [ 2.154832, 41.37332 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Escola Pau Vila", "TITULAR": "Pública", "LAT": 41.366941, "LON": 2.14273 }, "geometry": { "type": "Point", "coordinates": [ 2.14273, 41.366941 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Escola Pia Sant Antoni", "TITULAR": "Concertada", "LAT": 41.378003, "LON": 2.164366 }, "geometry": { "type": "Point", "coordinates": [ 2.164366, 41.378003 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Escola Poble-sec", "TITULAR": "Pública", "LAT": 41.370563, "LON": 2.165259 }, "geometry": { "type": "Point", "coordinates": [ 2.165259, 41.370563 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Escola Tres Pins", "TITULAR": "Pública", "LAT": 41.367015, "LON": 2.159924 }, "geometry": { "type": "Point", "coordinates": [ 2.159924, 41.367015 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Jesuïtes Poble Sec - Sant Pere Claver", "TITULAR": "Concertada", "LAT": 41.37296, "LON": 2.173451 }, "geometry": { "type": "Point", "coordinates": [ 2.173451, 41.37296 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Jesús Maria", "TITULAR": "Concertada", "LAT": 41.374269, "LON": 2.15532 }, "geometry": { "type": "Point", "coordinates": [ 2.15532, 41.374269 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Magòria", "TITULAR": "Concertada", "LAT": 41.371554, "LON": 2.14573 }, "geometry": { "type": "Point", "coordinates": [ 2.14573, 41.371554 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Maristes Anna Ravell", "TITULAR": "Concertada", "LAT": 41.372075, "LON": 2.161737 }, "geometry": { "type": "Point", "coordinates": [ 2.161737, 41.372075 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Sagrat Cor", "TITULAR": " Concertada", "LAT": 41.375527, "LON": 2.165693 }, "geometry": { "type": "Point", "coordinates": [ 2.165693, 41.375527 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Salesià de Sant Josep", "TITULAR": "Concertada", "LAT": 41.37744, "LON": 2.156006 }, "geometry": { "type": "Point", "coordinates": [ 2.156006, 41.37744 ] } },
+{ "type": "Feature", "properties": { "CENTRE": "Sant Francesc Xavier", "TITULAR": "Concertada", "LAT": 41.37257, "LON": 2.158082 }, "geometry": { "type": "Point", "coordinates": [ 2.158082, 41.37257 ] } }
+]
+}
+};
